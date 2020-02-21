a8f5a83ef31391c0b8f7ba043f8ed67a5e7e004a
fredieu <fredieunabia@gmail.com>
2020-02-21 21:27:13 +0000

    Add firebase hosting to project
    Amend generated files

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Under construction...coming soon!</p>
       </header>
     </div>
   );
